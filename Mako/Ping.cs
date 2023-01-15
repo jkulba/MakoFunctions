8e3bbb0c9db4fbde7250cc9d77c7e567e1f9d9eb
Jim Kulba <jkulba@gmail.com>
2023-01-14 18:24:08 -0800

    Added more info

--- Mako/Ping.cs
+++ Mako/Ping.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Mako
@@ -24,15 +25,38 @@ namespace Mako
         {
             _invocationId = executionContext.InvocationId.ToString();
 
+            var config = GetConfig(executionContext);
+            string appVersion = config.GetValue<string>("app_version");
+            string buildRun = config.GetValue<string>("build_run");
+            string buildCommitHash = config.GetValue<string>("build_commit_hash");
+            string buildBranch = config.GetValue<string>("build_branch");
+            string buildDate = config.GetValue<string>("build_date");
+            string builtBy = config.GetValue<string>("built_by");
+
             await Task.Yield();
 
             return (ActionResult)new OkObjectResult(new
             {
                 InvocationId = _invocationId,
-                Application = "Heimdall Functions",
+                Application = "Mako Functions",
                 Message = "Ping Response",
+                AppVersion = appVersion,
+                BuildRun = buildRun,
+                BuildCommitHash = buildCommitHash,
+                BuildBranch = buildBranch,
+                BuildDate = buildDate,
                 InvocationDate = DateTimeOffset.UtcNow
             });
         }
+
+        private static IConfigurationRoot GetConfig(ExecutionContext context)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("version.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
     }
 }
+
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Mako - Azure Functions App
+
+## Development setup
+
+1. Install Azure Functions Core Tools https://learn.microsoft.com/en-us/azure/azure-functions/
+
+1. If you are using Visual Studio Code - install Azure Functions Extension
